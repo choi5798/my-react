358a4b95c3edb3f2c610b1833a57a3be2c04b1ec
Choi Seongwon <vlflalektzja@naver.com>
2022-03-24 20:22:41 +0900

    Combine with one root element

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+  // return 에는 오직 하나의 root element만 있어야 한다.
+  // 즉 하나의 태그만 존재해야 함
+  // 여러 태그를 쓰고 싶으면 그 전체를 감싸는 하나의 태그가 필요함
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
