642c77ef7e78813e6813ab7e2f07a6d89221cbb9
Choi Seongwon <vlflalektzja@naver.com>
2022-03-27 01:18:33 +0900

    App.js를 줄이기 위한 새로운 컴포넌트 대규모 토목공사

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -21,32 +21,20 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
+    {
+      id: "e5",
+      title: "MacBook M1 PRO",
+      amount: 3000000,
+      date: new Date(2022, 2, 7),
+    },
   ];
 
   return (
     // 커스텀 컴포넌트는 대문자로 시작하는게 국룰!
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses expenses={expenses}/>
+
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,4 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
@@ -6,15 +7,10 @@ function ExpenseItem(props) {
   // 여러 태그를 쓰고 싶으면 그 전체를 하나의 태그로 감싸는 형태로 사용
   // html의 class속성 -> className으로 대체되었다. js에서도 class가 쓰이니깐.
   // html태그의 속성값을 전달 받을 때: props 사용!! (각 컴포넌트의 파라미터는 오직 한 개만 존재해야 함)
-  const year = props.date.getFullYear();
-  const month = props.date.toLocaleString("ko-KR", {month: 'long'});
-  const day = props.date.toLocaleString('ko-KR', {day:'2-digit'})
 
   return (
     <div className="expense-item">
-      <div>{year}년</div>
-      <div>{month}</div>
-      <div>{day}</div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
