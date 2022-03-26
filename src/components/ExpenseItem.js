193c5e548b7ad5b762f61f6819d1961cdcbdc61e
Choi Seongwon <vlflalektzja@naver.com>
2022-03-26 22:15:13 +0900

    주석 설명 추가

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,7 +5,7 @@ function ExpenseItem(props) {
   // 즉 하나의 태그만 존재해야 함
   // 여러 태그를 쓰고 싶으면 그 전체를 하나의 태그로 감싸는 형태로 사용
   // html의 class속성 -> className으로 대체되었다. js에서도 class가 쓰이니깐.
-  // html태그의 속성값을 전달 받을 때: props 사용!! (오직 한 개만 존재해야 함)
+  // html태그의 속성값을 전달 받을 때: props 사용!! (각 컴포넌트의 파라미터는 오직 한 개만 존재해야 함)
   
   return (
     <div className="expense-item">
