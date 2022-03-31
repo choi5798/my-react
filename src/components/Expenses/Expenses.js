e766e4093cd169ab5125959704a3459cd9f45b69
Choi Seongwon <vlflalektzja@naver.com>
2022-04-01 02:42:40 +0900

    Add filter with useState & 2-way binding. Finish

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
+import React, {useState} from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  // 실제로 dumb component(state가 없는 멍청이 컴포넌트)가 stateful componenet보다 훨씬 많이 쓰인다
+  // 컴포넌트들을 작은 조각들(병사)로 나누고 실제 데이터를 관리하는 것은 몇몇개의 stateful component들이다. (간부)
+  const [selectedYear, setSelectedYear] = useState('2021');
+  
+  const selectFilterHandler = (selectedYear) => {
+    setSelectedYear(selectedYear);
+  }
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={selectedYear} onSelectFilter = {selectFilterHandler}></ExpensesFilter>
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
